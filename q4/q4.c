73106ef0be5fb9504ed5d2b1e7d1180e83ff7958
pixelanikait <kulkarnianikait@gmail.com>
2026-04-04 17:50:00 +0530

    q4 ver 3 check

--- q4/q4.c
+++ q4/q4.c
@@ -6,22 +6,24 @@ int main() {
     char op[10];
     int a, b;
     while (scanf("%s %d %d", op, &a, &b) == 3) {
-        char libname[32] = "lib";
+        char libname[32] = "./lib";
         strcat(libname, op);
         strcat(libname, ".so");
         void *handle = dlopen(libname, RTLD_NOW);
-        if (!handle) return 1;
-
+        if (!handle) {
+            printf("0\n");
+            continue;
+        }
+        dlerror();
         int (*func)(int, int);
         *(void **)(&func) = dlsym(handle, op);
         if (!func) {
+            printf("0\n");
             dlclose(handle);
-            return 1;
+            continue;
         }
-
         int result = func(a, b);
         printf("%d\n", result);
-
         dlclose(handle);
     }
     return 0;
