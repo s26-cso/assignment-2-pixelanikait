ebbddf1cf95c100d6eb292fabf9ae894a9787967
pixelanikait <kulkarnianikait@gmail.com>
2026-04-04 17:20:57 +0530

    q4 ver 2check

--- q4/q4.c
+++ q4/q4.c
@@ -1,31 +1,28 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 #include <dlfcn.h>
 
 int main() {
     char op[10];
     int a, b;
-
     while (scanf("%s %d %d", op, &a, &b) == 3) {
-        char libname[20] = "lib";
+        char libname[32] = "lib";
         strcat(libname, op);
         strcat(libname, ".so");
-        void *handle = dlopen(libname, RTLD_LAZY);
-        if (!handle) {
-            fprintf(stderr, "Error loading %s\n", libname);
-            continue;
-        }   
-        int (*func)(int, int) = dlsym(handle, op);
+        void *handle = dlopen(libname, RTLD_NOW);
+        if (!handle) return 1;
+
+        int (*func)(int, int);
+        *(void **)(&func) = dlsym(handle, op);
         if (!func) {
-            fprintf(stderr, "Error finding function %s\n", op);
             dlclose(handle);
-            continue;
+            return 1;
         }
+
         int result = func(a, b);
         printf("%d\n", result);
+
         dlclose(handle);
-    
-    return 0;
     }
+    return 0;
 }
